b1ea1a85cd37fafc9c6eb2cd6794635c71b39ba0
gagu-PC\gagu <gagu@insowe.com>
2018-04-14 16:06:49 +0800

    no message

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const opencc = require('node-opencc');
-const fs = require('fs');
+const opencc = require("node-opencc");
+const fs = require("fs");
+const iconv = require("iconv-lite");
+const jschardet = require("jschardet");
 
-var text = fs.readFileSync('0.txt','utf8');
+var text = fs.readFileSync("0.txt");
+var encoding = jschardet.detect(text).encoding;
+text = iconv.decode(text, encoding);
 var text2 = opencc.simplifiedToTaiwan(text);
 //text2 = text2.replace(/[(\r\n)]{2,10}/g,"\r\n");
-text2 = text2.replace(/\r\n\s+/g,"\r\n");
-text2 = text2.split('').join(' ');
-text2 = text2.replace(/[(\r\n )]{3,10}/g,"\r\n\r\n");
+text2 = text2.replace(/\r\n\s+/g, "\r\n");
+text2 = text2.split("").join(" ");
+text2 = text2.replace(/[(\r\n )]{3,10}/g, "\r\n\r\n");
 
-if (fs.existsSync('2.txt')) {
-  fs.unlinkSync('2.txt');
+if (fs.existsSync("2.txt")) {
+  fs.unlinkSync("2.txt");
 }
 
-fs.writeFileSync('2.txt', text2, 'utf8');
+fs.writeFileSync("2.txt", text2, "utf8");
