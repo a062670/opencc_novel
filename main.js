a9447b682281c13f4788d67b49823d1e3e427031
gagu-home <gagu@insowe.com>
2022-04-23 18:12:01 +0800

    no message

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ var text = fs.readFileSync("0.txt");
 var encoding = jschardet.detect(text).encoding;
 text = iconv.decode(text, encoding);
 var text2 = opencc.simplifiedToTaiwan(text);
-//text2 = text2.replace(/[(\r\n)]{2,10}/g,"\r\n");
-text2 = text2.replace(/\r\n\s+/g, "\r\n");
-text2 = text2.split("").join(" ");
-text2 = text2.replace(/[(\r\n )]{3,10}/g, "\r\n\r\n");
+// text2 = text2.replace(/[(\r\n)]{3,10}/g, "\r\n\r\n");
+// text2 = text2.replace(/\r\n\s+/g, "\r\n");
+// text2 = text2.split("").join(" ");
+// text2 = text2.replace(/[(\r\n )]{3,10}/g, "\r\n\r\n");
 
 if (fs.existsSync("2.txt")) {
   fs.unlinkSync("2.txt");
